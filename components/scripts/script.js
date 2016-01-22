9b00726fd37ca24e3e956ce57273ea3cabfccf55
Mohammad Rebati <m.rebati@icloud.com>
2016-01-22 18:47:40 +0330

    top bar and nav-bar changes
    in this commit I added some animations and the search bar.

--- components/scripts/script.js
+++ components/scripts/script.js
@@ -1,48 +1,56 @@
-$(document).ready(function(){
+$(document).ready(function() {
   'use strict';
-  $('#questions').niceScroll();
-});
+  $('#search-input').hide();
+  $('html').niceScroll({
+    cursorcolor: '#333',
+    cursorborder: '0px',
+    cursoropacitymin: '0',
+    cursoropacitymax: '0.8'
+  });
+  $('#search-close').on('click', function() {
+    var menuitem = $('.menu-item');
+    var overlay = $('#search-curtain');
+    var searchbtn = $('#search-icon');
+    var closebtn = $('#search-close');
+    $('#search-input').removeClass('search-input-show');
+    $('#search-input').addClass('search-input-hide');
+    setTimeout(function() {
+      $('#search-input').hide();
+      menuitem.removeClass('search-show');
+      menuitem.addClass('search-hide');
+      overlay.fadeOut(300);
+      $('html').removeClass('full-html');
+      setTimeout(function() {
+        menuitem.show();
+      }, 300);
+        closebtn.addClass('scaledown');
+        setTimeout(function(){
+          closebtn.hide();
+          searchbtn.addClass('scaleup');
+        },200);
+    }, 300);
+  });
 
-$(function(){
-  'use strict';
-  $('#questions').niceScroll();
-});
-
-$('.nav-icon').first().click(function(){
-  'use strict';
-  var ul = $('.nav-bar > ul');
-  if(ul.hasClass('nav-clicked')){
-    ul.slideUp(function(){
-      ul.removeClass('nav-clicked');
-    });
-  }else{
-    ul.addClass('nav-clicked').slideDown();
-  }
-});
-
-$(window).resize(function(){
-  'use strict';
-  var width = $(window).width();
-  if(width > 650){
-    $('.nav-icon').hide();
-    $('.nav-bar > ul').removeClass('nav-clicked').show();
-  }
-  if(width <= 650){
-    $('.nav-icon').show();
-    $('.nav-bar > ul').hide();
-  }
-});
-
-$(window).scroll(function () {
-  'use strict';
-  if ($(this).scrollTop() > 167) {
-    $('.nav').first().css('position','fixed');
-    $('.nav').first().css('top','0');
-    $('.nav').first().css('width','100%');
-    /*$('.nav').first().css('background','rgba(0,0,0,0.3), url("../../../Content/images/header.jpg") repeat fixed 0 10% / cover ');
-    $('.main-header').css('height','40px').css('position','fixed');*/
-  }else{
-    $('.nav').first().removeAttr('style');
-    /*$('.main-header').removeAttr('style');*/
-  }
+  $('#search-icon').on('click', function() {
+    var menuitem = $('.menu-item');
+    var overlay = $('#search-curtain');
+    var searchbtn = $('#search-icon');
+    var closebtn = $('#search-close');
+    menuitem.removeClass('search-hide');
+    menuitem.addClass('search-show');
+    $('html').addClass('full-html');
+    overlay.fadeIn(300);
+    setTimeout(function() {
+      menuitem.hide();
+      $('#search-input').removeClass('search-input-hide');
+      $('#search-input').addClass('search-input-show');
+      setTimeout(function() {
+        $('#search-input').show();
+      }, 300);
+      searchbtn.addClass('scaledown');
+      setTimeout(function() {
+        closebtn.addClass('scaledown');
+      }, 200);
+    }, 300);
+  });
 });
